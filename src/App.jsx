3ca328610159ca87671da5ee52f53432401cfdeb
Pabmoy25 <pabmoy1725@gmail.com>
2024-01-29 17:19:41 -0300

    se agrega footer al proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import Ejercicio2 from "./components/Ejercicio2";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Ejercicio3 from "./components/Ejercicio3";
+import './App.css'
+import Footer from "./components/Footer"
+
 
 function App() {
   return (
+    <>
     <section className="container mt-5">
       <Ejercicio2 ejercicio2="my friend!"></Ejercicio2>
+      <Ejercicio3 ejercicio3="(from changed state)"> </Ejercicio3>
     </section>
+    <Footer></Footer>
+
+    </>
   );
 }
 
---
+++ src/components/Ejercicio3.jsx
@@ -0,0 +1,15 @@
+const Ejercicio3 = () => {
+
+    let frase = "(from changed state)"
+
+    return (
+        <article>
+            
+            <button className="btn btn-primary" onClick={frase}>Más</button>
+
+
+        </article>
+    );
+};
+
+export default Ejercicio3;
